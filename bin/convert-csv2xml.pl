7333cb6f1c90ce2ce7deb741b85b3e953da5449e
Erlend Leganger <erlend.leganger@gmail.com>
2018-03-29 00:42:15 +0200

    Use checksum of title as id

--- bin/convert-csv2xml.pl
+++ bin/convert-csv2xml.pl
@@ -1,5 +1,14 @@
 use strict;
 use Data::Dumper;
+
+sub cksum{
+   my $string=shift;
+   #return unpack("%64A*",$string)%65535 . length($string);
+   #33107 is a prime number
+   return unpack("%64A*",$string) * 33107 . length($string);
+}
+
+
 my %db;
 my $f="src/pcklst-data.csv";
 open IN,"<",$f or die "cannot open $f, stopping";
@@ -17,7 +26,8 @@ open OUT,'>:encoding(UTF-8)',$out or die("cannot create $out, stopping");
 print OUT "<items>\n";
 my $cnt=0;
 for my $item(sort keys %db){
-   my $id=sprintf("item%04d",$cnt++);
+   #my $id=sprintf("item%04d",$cnt++);
+   my $id=cksum($item);
    print OUT qq(\n   <item id="$id">\n);
    print OUT qq(      <title>$item</title>\n);
    print OUT qq(      <category>$db{$item}{category}</category>\n);
